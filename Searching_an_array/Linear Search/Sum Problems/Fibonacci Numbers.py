72fe7e716a335c082a1fa8f7c625025aa821ea8a
gbondos <44383636+gbondos@users.noreply.github.com>
2020-01-16 11:44:05 -0500

    Extra

--- Searching_an_array/Linear Search/Sum Problems/Fibonacci Numbers.py
+++ Searching_an_array/Linear Search/Sum Problems/Fibonacci Numbers.py
@@ -1,12 +1,12 @@
 def fibonacci_numbers(n):
     if n <0:
         return "Incorrect input of n"
-    if n <= 1:
+    if n <= 1: #if n ==0: return 0 or if n==1: return n
         return n
     else:
         return fibonacci_numbers(n-1) + fibonacci_numbers(n-2)
 
 def main():
-    n = 9
+    n = 10
     print(fibonacci_numbers(n))
 main()
